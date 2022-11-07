2d37471ca1ef6918e6d11c9885fba774fd691ed1
Yash <yashpande2002@gmail.com>
2022-11-08 03:07:14 +0530

    t-shirts me changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -53,7 +53,7 @@ export default function Cart() {
       </div>
       <button
         onClick={() => setShowCartItems(prev => !prev)}
-        className="fixed top-0 right-0 mr-4 mt-4 w-12 bg-blue-500 p-2  rounded-full text-white hover:bg-blue-700"
+        className="fixed top-0 right-0 mr-4 mt-4 w-12 bg-blue-500 p-2  rounded-full text-white hover:bg-blue-700 z-50"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -68,7 +68,7 @@ export default function Cart() {
             d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
           />
         </svg>
-        <div className="bg-red-500 rounded-full text-xs absolute w-6 h-6 flex justify-center items-center right-0 bottom-0 transform translate-x-2 translate-y-2">
+        <div className="bg-red-500 rounded-full text-xs absolute w-6 h-6 flex justify-center items-center right-0 bottom-0 transform translate-x-2 translate-y-2 z-50">
           {cart.length}
         </div>
       </button>
